e426039dc88bc50d6ffc0b2b365024fe98514f8c
J. Hathaway <hathawayj@gmail.com>
2020-12-07 11:01:51 -0700

    clean_tb_cases

--- data-raw/data4tuberculosis.R
+++ data-raw/data4tuberculosis.R
@@ -1,113 +1,61 @@
-pacman::p_load(tidyverse, DataPushR, fs)
+# Data Requests from Students
+library(tidyverse)
 
-#https://www.who.int/tb/country/data/download/en/
+# students should end up realizing they need the tb_cases file and that they want it in the form
 
-tb_cases <- read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=notifications")
-tb_budget <- read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=budget")
-tb_utilization <- read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=expenditure_utilisation")
-tb_outcomes <- read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=outcomes")
-tb_dictionary <- read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=dictionary")
-tb_estimates <- read_csv("https://extranet.who.int/tme/generateCSV.asp?ds=estimates")
-tb_rfds <- tidyr::who
+# |country     |g_whoregion | year|sex |age  | age_middle|var            | cases|
+# |:-----------|:-----------|----:|:---|:----|----------:|:--------------|-----:|
+# |Argentina   |AMR         | 2006|f   |65   |         75|extrapulmonary |    75|
+# |Puerto Rico |AMR         | 2015|f   |3544 |         39|relapse        |     2|
+# |Burundi     |AFR         | 2010|f   |4554 |         49|smear negative |    77|
+# |Ecuador     |AMR         | 2001|m   |65   |         75|smear positive |   116|
 
-## Build package
+tb_cases <- read_csv("https://github.com/byuidatascience/data4tuberculosis/raw/master/data-raw/tb_cases/tb_cases.csv")
 
-package_name_text <- "data4tuberculosis"
-base_folder <- "../../byuidatascience/"
-user <- "byuidatascience"
-package_path <- str_c(base_folder, package_name_text)
+tb_rfds <- read_csv("https://github.com/byuidatascience/data4tuberculosis/raw/master/data-raw/tb_rfds/tb_rfds.csv")
 
+# ep = 'extrapulmonary' - Extra-pulmonary tuberculosis (EPTB) defined as any bacteriologically confirmed or clinically diagnosed case of TB involving organs other than the lungs. 
 
-####  Run to create repo locally and on GitHub.  ######
+# Patients with sputum smear–negative TB are less infectious than patients with sputum smear–positive TB [1, 2]. Nevertheless, patients with smear-negative, culture-positive pulmonary TB are capable of transmitting M. tuberculosis [3, 4]
 
-github_info <- dpr_create_github(user, package_name_text)
+# sn = 'smear negative' 
+# sp = 'smear positive'
+# rel = 'relapse cases' - TB relapse is defined as a patient who has become (and remained) culture negative while receiving therapy but after completion of therapy becomes: Culture positive again or. Has clinical or radiographic deterioration that is consistent with active tuberculosis.
 
-package_path <- dpr_create_package(list_data = NULL,
-                                      package_name = package_name_text,
-                                      export_folder = base_folder,
-                                       git_remote = github_info$clone_url)
-
-##### dpr_delete_github(user, package_name_text) ####
-
-####### End create section
-github_info <- dpr_info_github(user, package_name_text)
-usethis::proj_set(package_path)
-
-usethis::use_data(tb_cases, tb_budget, tb_utilization, tb_outcomes, tb_rfds, tb_dictionary,tb_estimates, overwrite = TRUE)
-
-dpr_export(tb_cases, export_folder = path(package_path, "data-raw"), 
-           export_format = c(".csv", ".json", ".xlsx", ".sav", ".dta"))
-dpr_export(tb_budget, export_folder = path(package_path, "data-raw"), 
-           export_format = c(".csv", ".json", ".xlsx", ".sav", ".dta"))
-dpr_export(tb_utilization, export_folder = path(package_path, "data-raw"), 
-           export_format = c(".csv", ".json", ".xlsx", ".sav", ".dta"))
-dpr_export(tb_outcomes, export_folder = path(package_path, "data-raw"), 
-           export_format = c(".csv", ".json", ".xlsx", ".sav", ".dta"))
-dpr_export(tb_rfds, export_folder = path(package_path, "data-raw"), 
-           export_format = c(".csv", ".json", ".xlsx", ".sav", ".dta"))
-dpr_export(tb_dictionary, export_folder = path(package_path, "data-raw"), 
-           export_format = c(".csv", ".json", ".xlsx", ".sav", ".dta"))
-dpr_export(tb_estimates, export_folder = path(package_path, "data-raw"), 
-           export_format = c(".csv", ".json", ".xlsx", ".sav", ".dta"))
-
-
-dpr_document(tb_cases, extension = ".md.R", export_folder = usethis::proj_get(),
-             object_name = "tb_cases", 
-             title = "Word Health Organization (WHO) Tuberculosis case notifications by country",
-             description = "See source for description of the data. tb_dictionary describes the column names.",
-             source = "https://www.who.int/tb/country/data/download/en/",
-             var_details = list(nothing = "nothing"))
-
-dpr_document(tb_budget, extension = ".md.R", export_folder = usethis::proj_get(),
-             object_name = "tb_budget", 
-             title = "Word Health Organization (WHO) Tuberculosis budgets by country",
-             description = "See source for description of the data. tb_dictionary describes the column names.",
-             source = "https://www.who.int/tb/country/data/download/en/",
-             var_details = list(nothing = "nothing"))
-
-dpr_document(tb_utilization, extension = ".md.R", export_folder = usethis::proj_get(),
-             object_name = "tb_utilization", 
-             title = "Word Health Organization (WHO) Tuberculosis expenditures and utilization by country",
-             description = "See source for description of the data. tb_dictionary describes the column names.",
-             source = "https://www.who.int/tb/country/data/download/en/",
-             var_details = list(nothing = "nothing"))
-
-dpr_document(tb_outcomes, extension = ".md.R", export_folder = usethis::proj_get(),
-             object_name = "tb_outcomes", 
-             title = "Word Health Organization (WHO) Tuberculosis treatment outcomes by country",
-             description = "See source for description of the data. tb_dictionary describes the column names.",
-             source = "https://www.who.int/tb/country/data/download/en/",
-             var_details = list(nothing = "nothing"))
-
-dpr_document(tb_dictionary, extension = ".md.R", export_folder = usethis::proj_get(),
-             object_name = "tb_dictionary", 
-             title = "Word Health Organization (WHO) Tuberculosis csv file column names",
-             description = "File found at https://extranet.who.int/tme/generateCSV.asp?ds=dictionary",
-             source = "https://www.who.int/tb/country/data/download/en/",
-             var_details = list(nothing = "nothing"))
-
-dpr_document(tb_estimates, extension = ".md.R", export_folder = usethis::proj_get(),
-             object_name = "tb_estimates", 
-             title = "Word Health Organization (WHO) Tuberculosis csv file column names",
-             description = "File found at https://extranet.who.int/tme/generateCSV.asp?ds=dictionary",
-             source = "https://www.who.int/tb/country/data/download/en/",
-             var_details = list(nothing = "nothing"))
-
-dpr_document(tb_rfds, extension = ".md.R", export_folder = usethis::proj_get(),
-             object_name = "tb_rfds", 
-             title = "Word Health Organization (WHO) Tuberculosis as found in the R for Data Science Book",
-             description = "See the tidy data chapter https://r4ds.had.co.nz/tidy-data.html",
-             source = "https://tidyr.tidyverse.org/reference/who.html",
-             var_details = list(nothing = "nothing"))
-
-dpr_readme(usethis::proj_get(), package_name_text, user)
-
-
-dpr_write_script(folder_dir = package_path, r_read = "scripts/tuberculosis_package.R", 
-                 r_folder_write = "data-raw", r_write = str_c(package_name_text, ".R"))
-devtools::document(package_path)
-dpr_push(folder_dir = package_path, message = "'documentation'", repo_url = NULL)
-
-
+cases_clean <- tb_cases %>%
+  select(-(new_sp:c_newinc), 
+         -contains('_fu'), -contains('_mu'), -contains('_sexunk'), 
+         -contains('gesex'), -contains('15plus'), -contains('014'),
+         -(rdx_data_available:hiv_reg_new2)) %>%
+  pivot_longer(
+    cols = new_sp_m04:newrel_f65, 
+    names_to = "key", 
+    values_to = "cases", 
+    values_drop_na = TRUE
+  ) %>% 
+  mutate(
+    key = stringr::str_replace(key, "newrel", "new_rel")
+  ) %>%
+  separate(key, c("new", "var", "sexage")) %>% 
+  select(-new, -iso2, -iso3, -iso_numeric) %>% 
+  separate(sexage, c("sex", "age"), sep = 1) %>%
+  mutate(
+    age_middle = case_when(
+      age == '04' ~ 2,
+      age == '514' ~ 9,
+      age == '1524' ~ 19,
+      age == '2534' ~ 29,
+      age == '3544' ~ 39,
+      age == '4554' ~ 49,
+      age == '5564' ~ 59,
+      age == '65' ~ 75),
+    var = case_when(
+      var == 'sp' ~ 'smear positive',
+      var == 'sn' ~ 'smear negative',
+      var == 'rel' ~ 'relapse',
+      var == 'ep' ~ 'extrapulmonary'
+    )) %>%
+  select(country, g_whoregion, year, sex, age, age_middle, var, cases)
+    
 
 
